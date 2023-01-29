9f3f4e4ee14a3f2f9ff9799b738fc434977776cf
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-01-29 06:05:05 +0000

    Sayce 1st at Homework 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,22 @@
 ﻿//write include statements
-
+# include <iostream>
 
 //write namespace using statement for cout
+using namespace std;
 
+// prototype function
+int multiply_numbers(num1);
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+int num1;
+
 int main()
 {
-	
+num1=10;
+multiply_numbers(num1);	
 
 	return 0;
+
 }
