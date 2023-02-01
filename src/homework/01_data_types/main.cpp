eec1ea39ef4ed912ea9cdd35102af66d9c52c063
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-02-01 03:54:59 +0000

    Added 2 test cases to check multiply_number func

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,23 +1,27 @@
 ﻿//write include statements
 # include <iostream>
 # include "data_types.h"
-//write namespace using statement for cout
-using namespace std;
+//write namespace using statement for cout and cin
+using std::cout;
+using std::cin;
+using std::endl;
 
 // prototype function
 int multiply_numbers(int num1);
 
 /*
-Call multiply_numbers with 5 and 10 parameter values and display function result
+Call multiply_numbers with 5 and num parameter values and display function result
 */
 int num;
 int result_mult;
 
 int main()
 {
-num=10;
+cout << "Enter the number to multiply by 5  ";
+cin >> num;
+cout << "The number entered is  " << num <<"\n";
 result_mult = multiply_numbers(num);	
-cout<< result_mult;
+cout<< "Result of multplying num and 5 = "<< result_mult <<endl;
 	return 0;
 
 }
