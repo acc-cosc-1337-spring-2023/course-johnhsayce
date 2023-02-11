ebc599ea1a4101736dfc2c054f09e7c2391f1b4d
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-02-11 23:33:11 +0000

    Comleted meal rpcing program Sayce

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,7 @@
-﻿//example
+﻿//write function type here
 
 double get_sales_tax(double meal_amount);
 double get_tip_amount(double meal_amount, double TAX_RATE);
-//write function prototype here
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,17 +1,19 @@
 ﻿//write include statements
 # include <iostream>
+# include <iomanip>
 # include "hwexpressions.h"
-//write namespace using statement for cout and cin and endl
+//write namespace using statement for cout,cin, endl, fixed, setprecision
 using std::cout;
 using std::cin;
 using std::endl;
-const double TAX_RATE = 0.675;
+using std::fixed;
+using std::setprecision;
 
-//protoype functions
 double get_sales_tax(double meal_amount);
 double get_tip_amount(double meal_amount, const double tip_rate);
 /*
 Call the sales tax and tip amount functions
+and print out various totals
 */
 int main()
 {
@@ -19,13 +21,19 @@ int main()
 	double sales_tax;
 	double tip_rate;
 	double tip_amount;
-	cout<<" Enter the amount of the meal";
+	cout<<" Enter the amount of the meal $ ";
 	cin>>meal_amount;
-	cout<<" The amount of the meal entered was S";
+	cout<<" The amount of the meal entered was $ "<<meal_amount<<endl;
 	sales_tax = get_sales_tax( meal_amount);
-	cout<< "The calculated sales tax is $"<< sales_tax;
-	cout<<" Enter the % of tip you would like to give";
+	cout<< " The calculated sales tax is $"<< sales_tax<<endl;
+	cout<<" Enter the % of tip you would like to give in % ";
 	cin>>tip_rate;
 	tip_amount = get_tip_amount( meal_amount,  tip_rate);
-	cout<<"The tip calculated based on the tip rate entered is $"<< tip_amount;
+	cout<<" The tip calculated based on the tip rate entered is $"<< tip_amount<<endl;
+	cout<<fixed<<setprecision(2)<<"  The final receipt for your order"<<endl;
+	cout<<fixed<<setprecision(2)<<"   Meal Price  $"<<meal_amount<<endl;
+	cout<<fixed<<setprecision(2)<<"   Tax amount $"<<sales_tax<<endl;
+	cout<<fixed<<setprecision(2)<<"   Tip amount $"<<tip_amount<<endl;
+	cout<<fixed<<setprecision(2)<<"  Total $"<<meal_amount+sales_tax+tip_amount<<endl;
+
 }
