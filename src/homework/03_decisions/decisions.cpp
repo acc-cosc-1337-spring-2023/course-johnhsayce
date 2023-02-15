2e775bb4357a4784249e888ed668bd4d99cf21ea
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-02-15 03:53:13 +0000

    Initial program for grade conversion

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,20 @@
 //write include statement for decisions header
-
+# include "decisions.h"
+# include <string>
 
 //Write code for function(s) code here
+std::string get_letter_grade_using_if(int grade)
+{
+  std::string letter_grade;  
+    
+    if (grade >=90 && grade<=100)
+        {return letter_grade="A";}
+            else if (grade>=80 && grade < 90)
+                {return letter_grade="B";}
+                    else if (grade>=70 && grade < 80)
+                        {return letter_grade="C";}
+                            else if (grade>=60 && grade < 69)
+                                {return letter_grade="D";}
+                                    else if (grade>=0 && grade < 59)
+                                        {return letter_grade="F";}
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,28 @@
 //write include statements
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include "decisions.h"
+//write namespace using statement for cout,cin, endl, fixed, setprecision
+using std::cout;
+using std::cin;
+using std::endl;
+
+std::string get_letter_grade_using_if(int grade);
+
 
 int main() 
 {
-	return 0;
+std::string letter_grade;
+int grade;
+cout<<" Enter the students numerical grade  ";
+cin>>grade;
+if(grade > 100)
+{
+cout<<" Incorrectly eneter, Enter the students numerical grade  ";
+cin>>grade;	
+}
+letter_grade = get_letter_grade_using_if(grade);
+cout<<" The students letter grade =  "<<letter_grade;
+return 0;
 }
