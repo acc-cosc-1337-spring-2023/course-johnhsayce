402f99cf2d1f648537654bff1eb8c5c1e7fbeba7
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-02-18 13:36:50 +0000

    Added items to letter grade program

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -18,3 +18,24 @@ std::string get_letter_grade_using_if(int grade)
                                     else if (grade>=0 && grade < 59)
                                         {return letter_grade="F";}
 }
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string letter_grade;
+    switch(grade){
+    case 90 ... 100:
+        return letter_grade="A";
+    break;
+    case 80 ... 89:
+        return letter_grade="B";
+    break;
+    case 70 ... 79:
+        return letter_grade="C";
+    break;
+    case 60 ... 69:
+        return letter_grade="D";
+    break;
+    case 0 ... 59:
+        return letter_grade="F";
+    break;
+    }
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -9,20 +9,27 @@ using std::cin;
 using std::endl;
 
 std::string get_letter_grade_using_if(int grade);
-
+std::string get_letter_grade_using_switch(int grade);
 
 int main() 
 {
+int menu_num;
+cout<<" ----MENU---- \n";
+cout<<" 1-Letter grade using if \n";
+cout<<" 2-Letter grade using switch \n";
+cout<<" 3-Exit \n";
+cin>>menu_num;
+
 std::string letter_grade;
 int grade;
 cout<<" Enter the students numerical grade  ";
 cin>>grade;
 if(grade > 100)
 {
-cout<<" Incorrectly eneter, Enter the students numerical grade  ";
+cout<<" Incorrectly entered value, Enter the students numerical grade  ";
 cin>>grade;	
 }
 letter_grade = get_letter_grade_using_if(grade);
-cout<<" The students letter grade =  "<<letter_grade;
+cout<<" The students letter grade =  "<<letter_grade<<"\n";
 return 0;
 }
