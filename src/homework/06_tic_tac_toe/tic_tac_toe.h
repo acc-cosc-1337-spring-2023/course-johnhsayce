afe999d0a34640bcf7e19f14d4653950be0d9dc2
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-03-21 16:42:50 +0000

    set up class structure for TTT game

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,32 @@
-//h
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <cstring>
+//using std::string, std::vector;
+using namespace std;
+
+
+class TicTacToe
+
+{
+
+
+private:
+void set_next_player();
+bool check_board_full();
+void clear_board();
+string player;
+vector<string> pegs{"","","","","","","","",""};
+
+
+public:
+TicTacToe();
+bool game_over();
+void start_game(string);
+void mark_board(int);
+string get_player();
+void display_board();
+
+
+};
