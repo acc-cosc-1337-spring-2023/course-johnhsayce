c3aebb6048f74c5956b51bda000e31ad225f727f
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-03-23 17:45:10 +0000

    made final chnages to test case for TTT

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,13 +1,41 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include"tic_tac_toe.h"
-class TicTacToe;
-TicTacToe game1;
+
+
 TEST_CASE("Verify Test Configuration", "verification")
 {
 	REQUIRE(true == true);
 }
 TEST_CASE("Game Over function")
  {
+	TicTacToe game1;
+
+	game1.mark_board(1);
 	REQUIRE(game1.game_over() == false);
+
+	game1.mark_board(2);
+	REQUIRE(game1.game_over() == false);
+
+	game1.mark_board(3);
+	REQUIRE(game1.game_over() == false);
+
+	game1.mark_board(4);
+	REQUIRE(game1.game_over() == false);	
+
+	game1.mark_board(5);
+	REQUIRE(game1.game_over() == false);
+
+	game1.mark_board(6);
+	REQUIRE(game1.game_over() == false);
+
+	game1.mark_board(7);
+	REQUIRE(game1.game_over() == false);
+
+	game1.mark_board(8);
+	REQUIRE(game1.game_over() == false);	
+
+	game1.mark_board(9);
+	REQUIRE(game1.game_over() == true);
+
 }
