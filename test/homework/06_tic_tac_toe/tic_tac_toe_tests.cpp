39456f8716ee91ba659ac34211a803953ae90af7
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-04-06 01:04:53 +0000

    Made change to total win test case

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,12 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include"tic_tac_toe.h"
+#include"tic_tac_toe_manager.h"
 
 
 TEST_CASE("Verify Test Configuration", "verification")
 {
 	REQUIRE(true == true);
 }
+
+TEST_CASE("Tests correct win total for get winner total X")
+ {
+	TicTacToeManager manage;
+	string player = "X";
+	TicTacToe game;
+	game.start_game(player);
+	manage.save_game(game);
+	REQUIRE(game.get_player() == "X");
+ }
+
+
+
 
 TEST_CASE("Tests first player is set to  X")
  {
