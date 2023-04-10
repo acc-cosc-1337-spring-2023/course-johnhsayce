24487e3c960512e2d2888c97d563b0f255192af8
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-04-10 16:33:20 +0000

    added setup items for the derived classes

--- src/homework/06_tic_tac_toe/tic_tac_toe_3.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_3.h
@@ -1,1 +1,18 @@
-//h
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <cstring>
+#include <memory>
+#include<stdlib.h>
+#include"tic_tac_toe.h"
+using std::string, std::vector;
+using namespace std;
+
+#ifndef TICTACTOE3_H
+#define TICTACTOE3_H
+
+class TicTacToe_3: public TicTacToe
+{
+};
+#endif
--- src/homework/06_tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,18 @@
-//h
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <cstring>
+#include <memory>
+#include<stdlib.h>
+#include"tic_tac_toe.h"
+using std::string, std::vector;
+using namespace std;
+
+#ifndef TICTACTOE4_H
+#define TICTACTOE4_H
+
+class TicTacToe_4: public TicTacToe
+{
+};
+#endif
