cfd0f06877441fce91390438fc58b6cc63859558
johnhsayce <123427727+johnhsayce@users.noreply.github.com>
2023-04-13 14:35:19 +0000

    made change to TTT4 cpp tofix check of O win

--- src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
@@ -11,22 +11,22 @@ using std::string, std::vector,std::cin, std::cout;
 bool TicTacToe_4::check_row_win()
 {
     if(((pegs[0]=="X") && (pegs[1] =="X") && (pegs[2] == "X") && (pegs[3]=="X"))
-    ||((pegs[0]=="O") && (pegs[1] =="O") && (pegs[2] == "O") && (pegs[3]=="X")))
+    ||((pegs[0]=="O") && (pegs[1] =="O") && (pegs[2] == "O") && (pegs[3]=="O")))
     {
     return true;
     }
     if(((pegs[4]=="X") && (pegs[5] =="X") && (pegs[6] == "X")&&(pegs[7]=="X"))
-    ||((pegs[4]=="O") && (pegs[5] =="O") && (pegs[6] == "O")&&(pegs[7]=="X")))
+    ||((pegs[4]=="O") && (pegs[5] =="O") && (pegs[6] == "O")&&(pegs[7]=="O")))
     {
     return true;
     }
     if(((pegs[8]=="X") && (pegs[9] =="X") && (pegs[10] == "X")&&(pegs[11]=="X"))
-    ||((pegs[8]=="O") && (pegs[9] =="O") && (pegs[10] == "O")&&(pegs[11]=="X")))
+    ||((pegs[8]=="O") && (pegs[9] =="O") && (pegs[10] == "O")&&(pegs[11]=="O")))
     {
     return true;
     }
     if(((pegs[12]=="X") && (pegs[13] =="X") && (pegs[14] == "X")&&(pegs[15]=="X"))
-    ||((pegs[12]=="O") && (pegs[13] =="O") && (pegs[14] == "O")&&(pegs[15]=="X")))
+    ||((pegs[12]=="O") && (pegs[13] =="O") && (pegs[14] == "O")&&(pegs[15]=="O")))
     {
     return true;
     }
@@ -36,22 +36,22 @@ return false;
 bool TicTacToe_4::check_col_win()
 {
     if(((pegs[0]=="X") && (pegs[4] =="X") && (pegs[8] == "X") && (pegs[12]=="X"))
-    ||((pegs[0]=="O") && (pegs[4] =="O") && (pegs[8] == "O") && (pegs[12]=="X")))
+    ||((pegs[0]=="O") && (pegs[4] =="O") && (pegs[8] == "O") && (pegs[12]=="O")))
     {
     return true;
     }
     if(((pegs[1]=="X") && (pegs[5] =="X") && (pegs[9] == "X")&&(pegs[13]=="X"))
-    ||((pegs[1]=="O") && (pegs[5] =="O") && (pegs[9] == "O")&&(pegs[13]=="X")))
+    ||((pegs[1]=="O") && (pegs[5] =="O") && (pegs[9] == "O")&&(pegs[13]=="O")))
     {
     return true;
     }
     if(((pegs[2]=="X") && (pegs[6] =="X") && (pegs[10] == "X")&&(pegs[14]=="X"))
-    ||((pegs[2]=="O") && (pegs[6] =="O") && (pegs[10] == "O")&&(pegs[14]=="X")))
+    ||((pegs[2]=="O") && (pegs[6] =="O") && (pegs[10] == "O")&&(pegs[14]=="O")))
     {
     return true;
     }
     if(((pegs[3]=="X") && (pegs[7] =="X") && (pegs[11] == "X")&&(pegs[15]=="X"))
-    ||((pegs[3]=="O") && (pegs[7] =="O") && (pegs[11] == "O")&&(pegs[15]=="X")))
+    ||((pegs[3]=="O") && (pegs[7] =="O") && (pegs[11] == "O")&&(pegs[15]=="O")))
     {
     return true;
     }
@@ -61,12 +61,12 @@ return false;
 bool TicTacToe_4::check_diag_win()
 {
     if(((pegs[0]=="X") && (pegs[5] =="X") && (pegs[10] == "X") && (pegs[15]=="X"))
-    ||((pegs[0]=="O") && (pegs[5] =="O") && (pegs[10] == "O") && (pegs[15]=="X")))
+    ||((pegs[0]=="O") && (pegs[5] =="O") && (pegs[10] == "O") && (pegs[15]=="O")))
     {
     return true;
     }
     if(((pegs[3]=="X") && (pegs[6] =="X") && (pegs[9] == "X")&&(pegs[12]=="X"))
-    ||((pegs[3]=="O") && (pegs[6] =="O") && (pegs[9] == "O")&&(pegs[12]=="X")))
+    ||((pegs[3]=="O") && (pegs[6] =="O") && (pegs[9] == "O")&&(pegs[12]=="O")))
     {
     return true;
     }
